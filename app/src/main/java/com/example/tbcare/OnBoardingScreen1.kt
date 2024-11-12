93332087038c37c739ce93ea592ada75634f9ffa
ahmad <azurenein@gmail.com>
2024-11-12 17:33:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tbcare/OnBoardingScreen1.kt
@@ -0,0 +1,11 @@
+package com.example.tbcare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnBoardingScreen1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding_screen1)
+    }
+}
---
+++ app/src/main/java/com/example/tbcare/SplashScreeen.kt
@@ -0,0 +1,26 @@
+package com.example.tbcare
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+
+class SplashScreeen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screeen)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, OnBoardingScreen1::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
