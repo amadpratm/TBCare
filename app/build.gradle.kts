909ffaebfc16ad83099fe8a920f53e27fcfeeeaa
ahmad <azurenein@gmail.com>
2024-11-17 19:29:24 +0700

    onb

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,5 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("me.relex:circleindicator:2.1.6")
 }
